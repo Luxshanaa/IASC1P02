0735d6de747650bf9f23225c280e66de81f4ccf3
Luxshanaa <ls18fz@brocku.ca>
2021-12-08 23:52:18 -0500

    Assignment4

---
+++ IASC1P02/Assignment4/Assignment4.js
@@ -0,0 +1,19 @@
+function mainPageOne(){
+  location.href="Assignment4Page1.html";
+}
+
+function mainPageTwo(){
+  location.href="Assignment4Page1.html";
+}
+
+function mainPageThree(){
+  location.href="Assignment4Page1.html";
+}
+
+/*Reference:
+https://stackoverflow.com/questions/16562577/how-can-i-make-a-button-redirect-my-page-to-another-page
+https://www.w3schools.com/jsref/jsref_setdate.asp*/
+
+function send(){
+  document.getElementById("thankYou").innerHTML= "Thank You!";
+}
